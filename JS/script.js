bdb409813ef1ec83c5ef6f4e8fdc0205a2de101f
ojasiiitd <ojassaxena03@gmail.com>
2019-06-14 11:19:54 +0530

    Changes made to data and AJAX request

--- JS/script.js
+++ JS/script.js
@@ -1,34 +1,48 @@
-function AJAXutil(url)
-{
-    function validateAJAX()
-    {
-        if (this.readyState == 4 && this.status == 200)
-        {
-            var data = JSON.parse(this.responseText);
-            main1(data)
-        }
-    };
-    var myReq = new XMLHttpRequest();
-    myReq.onreadystatechange = validateAJAX;
-    myReq.open("GET", url , true);
-    myReq.send();
-}
+var rand = document.getElementById("baxa");
+var btn = document.getElementById("btn");
+var pressed = 0;
 
-function main1(data)
+btn.addEventListener("click" , function() 
 {
-    var press = document.getElementById("btn"),
-        box = document.getElementById("baxa");
-    
-    press.addEventListener("click" , addRandom);
-
-    function addRandom()
+    pressed++;
+    if(pressed === 4)
     {
-        box.innerHTML += data.title + "<br>";
+        btn.classList.replace("is-link" , "is-danger");
+        pressed = 1;
     }
-}
+    var url = "https://learnwebcode.github.io/json-example/animals-" + pressed + ".json";
+    var get = new XMLHttpRequest();
+    get.open("GET" , url);
+    get.onload = function() 
+    {
+        if(get.status >= 200 && get.status < 400)
+        {
+            var dataRec = JSON.parse(get.responseText);
+            dispRandom(dataRec);
+        }
+        // else
+        // {
+        //     console.log("Site error");
+        // }
+    };
+    // get.onerror = function() {
+    //     console.log("Server error");
+    // }
+    get.send();
+});
 
-var url = "https://jsonplaceholder.typicode.com/todos/";
-for(var i=1 ; i<=10 ; i++)
+function dispRandom(data)
 {
-    AJAXutil(url + i);
+    var str = "";
+    for(var i=0 ; i<data.length ; i++)
+    {
+        str += "The name of my " + data[i]["species"] + " is <b>" + data[i]["name"] + "</b> and he likes :- <br>";
+        for(var j=0 ; j<data[i]["foods"]["likes"].length ; j++)
+            str += data[i]["foods"]["likes"][j] + ", ";
+        str += "<br> but he dislikes :- <br>";
+        for(var j=0 ; j<data[i]["foods"]["dislikes"].length ; j++)
+            str += data[i]["foods"]["dislikes"][j] + ", ";
+        str += "<br><br>";
+    }
+    rand.innerHTML += str;
 }
