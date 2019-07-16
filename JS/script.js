12fad9116e7385a9b15e7209e6567a3007983cec
ojasiiitd <ojassaxena03@gmail.com>
2019-07-16 13:45:56 +0530

    GET request update

--- JS/script.js
+++ JS/script.js
@@ -19,42 +19,37 @@ btn.addEventListener("click" , function()
             var dataRec = JSON.parse(get.responseText);
             dispRandom(dataRec);
         }
-        else
-        {
-            console.log("Site error");
-        }
     };
-    get.onerror = function() {
-        console.log("Server error");
-    }
     get.send();
 });
 
 function dispRandom(data)
 {
     var posts = data["data"]["children"];
-    console.log(posts[0]);
+
     for(var i=0 ; i<posts.length ; i++)
     {
         var curpost = posts[i]["data"];
+        var postLink = curpost["url"]
+        console.log(curpost);
 
         var nextPost = document.createElement("div");
         nextPost.className = "list-item";
         
         var link = document.createElement("a");
         link.className = "is-size-4";
-        link["href"] = "https://www.reddit.com" + curpost["permalink"];
+        link["href"] = postLink;
         link.innerHTML = "Post #" + (i+1) + " by : r/" + curpost["author"];
         nextPost.appendChild(link);
         
         var basicInfo = document.createElement("p");
         var str = "Created : " + unixTimeConverter(curpost["created"]) + "<br>";
         str += "Type : " + (curpost["link_flair_css_class"] || "Miscellaneous") + "<br>";
-        str += "Upvotes : " + curpost["score"] + "<br>";
         str += "Description : " + (curpost["title"] || "None") + "<br>";
+        str += "Upvotes : " + curpost["score"] + "<br>";
         basicInfo.innerHTML = str;
         nextPost.appendChild(basicInfo);
-        
+
         list.appendChild(nextPost);
     }
     gotData = true;
